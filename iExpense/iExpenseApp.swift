b2d940e42b32ab40b270df1153cf5a9e89e0f324
Tugran Yayla <tugrandev@gmail.com>
2023-04-03 14:20:13 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tuğran on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
